a1aa1c786129ce5d260528ece32dc3dfc3542b41
Jhenkar18 <Jhenkar.s.k183@gmail.com>
2024-12-04 12:25:03 +0530

    Adding Project files

---
+++ hai.js
@@ -0,0 +1,13 @@
+// function left(){
+//     return console.log("Left");
+// }
+
+// function right(){
+//     return console.log("Right");
+// }
+
+// left() || right();
+
+console.log(1!== true);
+console.log(1 != true);
+console.log(NaN != NaN);
